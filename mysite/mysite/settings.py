6a9a1de0734c7c3f5450379882e6c817df3f55ff
KirillSamokhin <90692078+KirillSamokhin@users.noreply.github.com>
2024-09-16 12:24:44 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok-free.app']
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
